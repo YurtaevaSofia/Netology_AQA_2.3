fac01204185f630c442330fcb4b79925645d4918
coursar <Sonia@MacBook-Air-Sofa.local>
2020-08-18 09:48:22 +0300

    Expected data changed for test checking, should fall

--- src/test/java/PostmanEchoTest.java
+++ src/test/java/PostmanEchoTest.java
@@ -15,7 +15,7 @@ class PostmanEchoTest {
 // Проверки
                     .then()
                     .statusCode(200)
-                    .body("data", equalTo("lalala"))
+                    .body("data", equalTo("ololo"))
             ;
         }
         }
